d840740fa19ec82480c2f3a48b09dfb3015f6df7
Dave Guarino <daguar@gmail.com>
2013-01-18 11:56:41 -0800

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SouthBendBusWidget::Application.config.secret_token = 'dea4a1e65d16da1aefdb5539de90fc9d9e89a69e5e93ab7e383287ea29cfc75b591e5ea06b7e9443f09ded5d93efbb38df24ad8cc62e269f73914f4abd3e8ac1'
