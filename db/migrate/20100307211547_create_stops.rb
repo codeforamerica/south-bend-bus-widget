01439bc213f1f140bbbf6921b8ab9ac4c3cf42f6
Dave Guarino <daguar@gmail.com>
2013-01-25 16:16:49 -0800

    Relax not_null constraint on stop lat and long so that real GTFS data will actually load

--- db/migrate/20100307211547_create_stops.rb
+++ db/migrate/20100307211547_create_stops.rb
@@ -4,8 +4,8 @@ class CreateStops < ActiveRecord::Migration
       t.string :stop_id
       t.string :stop_name
       t.string :stop_description
-      t.decimal :stop_lat, :precision => 15, :scale => 9, :null => false
-      t.decimal :stop_lon, :precision => 15, :scale => 9, :null => false
+      t.decimal :stop_lat, :precision => 15, :scale => 9#, :null => false
+      t.decimal :stop_lon, :precision => 15, :scale => 9#, :null => false
       t.string :stop_street
       t.string :stop_city
       t.string :stop_region
